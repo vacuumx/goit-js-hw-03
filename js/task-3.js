77a0d28b111a5085881cd83889a0080fd5062957
vacuumx <121231944+vacuumx@users.noreply.github.com>
2023-12-25 12:35:04 +0200

    goit-js-hw-03

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,16 @@
 "use strict";
-function checkForSpam(message){
-    const word1 = "SPAM";
-    const word2 = "SALE";
-    if(message.toUpperCase().includes(word1) || message.toUpperCase().includes(word2)){
-        return true;
+function filterArray(numbers, value){
+    const newArr = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (array[i]>value) {
+            newArr.push(array[i]);
+        }
     }
-    return false;
+    return newArr;
 };
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
